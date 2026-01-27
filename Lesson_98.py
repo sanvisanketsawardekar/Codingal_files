dc945880c2a70f838ddb426bb22493c74c69c01a
Sanvi Sawardekar <120212383+sanvisanketsawardekar@users.noreply.github.com>
2026-01-27 17:04:56 +0530

    Add files via upload

---
+++ Lesson_98.py
@@ -0,0 +1,36 @@
+### Activity 1
+print("Welcome to the world of programming!")
+
+
+print(4)
+
+
+print("Hello Python \n")
+
+print("hello",5)
+
+print("welcome to ", end= "*")
+
+
+##Activity2
+
+X = 5
+y = "John"
+
+print(X)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
+
+
+# Acticvityt3
+
+
+import keyword
+print("python keywords are",keyword.kwlist)
